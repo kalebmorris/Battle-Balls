5484f525865530a383a5397899d8740f4c4592d3
Hannah Leou <hleou@stanford.edu>
2018-10-14 14:59:20 -0700

    timer

---
+++ CS146-AssignThree/Assets/Scripts/ClockScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ClockScript : MonoBehaviour
+{
+
+    public int timeRemaining;
+    public Text clock;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int seconds = (timeRemaining % 60);
+        int minutes = (timeRemaining - seconds) / 60;
+        clock.text = minutes.ToString("0") + ":" + seconds.ToString("0");
+
+        if (timeRemaining <= 0)
+        {
+            StopCoroutine("LoseTime");
+            clock.text = "Time's Up";
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeRemaining--;
+        }
+    }
+
+}
